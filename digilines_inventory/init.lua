aabf7ec57595d849fa18d631edec9d53bd7251e2
electrodude <electrodude512@gmail.com>
2015-12-18 00:01:02 -0500

    Added missing local variable
    The on_metadata_inventory_take method of digiline_inventory:chest was
    broken, since it used a `channel` local variable that was never defined,
    but should have been fetched from the node's meta

--- digilines_inventory/init.lua
+++ digilines_inventory/init.lua
@@ -127,6 +127,7 @@ minetest.register_node("digilines_inventory:chest", {
 		minetest.log("action", player:get_player_name().." puts stuff into chest at "..minetest.pos_to_string(pos))
 	end,
 	on_metadata_inventory_take = function(pos, listname, index, stack, player)
+		local channel = minetest.get_meta(pos):get_string("channel")
 		local inv = minetest.get_meta(pos):get_inventory()
 		if inv:is_empty(listname) then
 			sendMessage(pos, "empty", channel)
