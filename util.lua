bba29f895c4fc8bdb9400f8469ffb4b55959bdf9
OgelGames <olliverdc28@gmail.com>
2026-06-09 15:30:33 +1000

    line length limits ugh

--- util.lua
+++ util.lua
@@ -1,10 +1,12 @@
 digilines.addPosRule = function(p, r)
-	core.log("deprecated", "Call to deprecated function 'digilines.addPosRule', use 'vector.add' instead.", 2, true)
+	core.log("deprecated",
+		"Call to deprecated function 'digilines.addPosRule', use 'vector.add' instead.", 2, true)
 	return vector.add(p, r)
 end
 
 digilines.cmpPos = function(p1, p2)
-	core.log("deprecated", "Call to deprecated function 'digilines.cmpPos', use 'vector.equals' instead.", 2, true)
+	core.log("deprecated",
+		"Call to deprecated function 'digilines.cmpPos', use 'vector.equals' instead.", 2, true)
 	return vector.equals(p1, p2)
 end
 
